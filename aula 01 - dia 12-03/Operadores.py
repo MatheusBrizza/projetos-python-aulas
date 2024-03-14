47d6e9aa4fd37671e1a6c66d53e7b7d433e41a59
182300645 <matheusbj95@gmail.com>
2024-03-13 21:39:19 -0300

    correção da aula 01

--- aula 01 - dia 12-03/Operadores.py
+++ aula 01 - dia 12-03/Operadores.py
@@ -1,13 +1,21 @@
 #Tipos de operadores
 
+# Python provides two types of division – float and integer. The / operator is used for float division,
+# while the // operator is used for integer division. For example, 10 / 3 results in 3.3333333333333335 (float division),
+# while 10 // 3 results in 3 (integer division)
+
+
 soma = 10 + 30
 diferença = 49 - 34
 multiplicacao = 7 * 10
-divisao = 10 / 2
+# divisao = 10 / 2
+divisaoFloat = 10 / 2
+divisaoInt = 10 // 2
 resto = 10 % 3
 
 # para converter uma variável em outro tipo, ou seja cast
-int(divisao)
-print(type(divisao))
-print (soma, diferença, multiplicacao, divisao, resto)
+# como visto nos comentários acima, não precisa usar cast(e nem consegue) de int para divisão com uma / 
+# int(divisao)
+#print(type(divisao))
+print (soma, diferença, multiplicacao, divisaoFloat, divisaoInt, resto)
 
