8c5eb0d1afcc9eccba3400dbfc11f532521f09ca
182300645 <matheusbj95@gmail.com>
2024-03-14 21:36:25 -0300

    exercício 7 dia 3 feito

--- aula 03 - dia 14-03/7. Jogo de adivinhação.py
+++ aula 03 - dia 14-03/7. Jogo de adivinhação.py
@@ -1,3 +1,9 @@
 # Crie um jogo de adivinhação onde o usuário tenta adivinhar um número secreto 
 # usando while
 
+numero = int(input("Digite um número: "))
+
+while numero != 5:
+    print("Você errou!")
+    numero = int(input("Digite um número: "))
+print("Você acertou!")
