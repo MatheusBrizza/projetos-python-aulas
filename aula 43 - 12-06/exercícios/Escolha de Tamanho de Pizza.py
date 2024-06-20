01f08264bf8b21eda130414c241900a1fd6a4e4c
182300645 <matheusbj95@gmail.com>
2024-06-19 21:45:24 -0300

    conexão com banco de dados funcionando

--- aula 43 - 12-06/exercícios/Escolha de Tamanho de Pizza.py
+++ aula 43 - 12-06/exercícios/Escolha de Tamanho de Pizza.py
@@ -7,6 +7,76 @@ tamanho.
 '''
 import tkinter as tk
 from tkinter import messagebox
+import datetime
+import mysql.connector
+
+nome_db = "senac_pizzaria"
+tb_pedidos = "pedidos"
+tb_clientes = "clientes"
+
+connector = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password=''
+    )
+
+# Verifica se o banco de dados senac_pizzaria existe
+executor_comando_sql = connector.cursor()
+'''executor_comando_sql.execute('SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = "senac_pizzaria";') # busca o banco de dados pelo nome
+nome_db = executor_comando_sql.fetchone()[0] # salva o resultado numa variável nome_db.'''
+executor_comando_sql.execute(f'SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = "{nome_db}";')
+
+# Salvando o resultado da pesquisa acima numa variável
+resultado_db = executor_comando_sql.fetchone()[0]
+
+# Fecha a conexão com o banco de dados
+connector.close()
+
+# Se o número de resultados for maior que zero, significa que o banco de dados existe
+if resultado_db > 0:
+  print(f'O banco de dados {nome_db} existe e esta pronto para uso.')
+else:
+    print(f"banco de dados {nome_db} não encontrado, criando-o agora.")
+    # Conectar-se ao servidor MySQL para criar o banco de dados
+    connector = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password=''
+    )
+
+    executor_comando_sql = connector.cursor() # necessário reiniciar a variável pois senão dá o erro ReferenceError: weakly-referenced object no longer exists pois a conexão foi terminada na utilização anterior
+    executor_comando_sql.execute(f'CREATE DATABASE {nome_db}')
+    connector.commit()
+    
+
+# Conectar-se ao banco de dados senac_pizzaria recém-criado
+connector = mysql.connector.connect(
+    host='127.0.0.1',
+    user='root',
+    password='',
+    database= nome_db  
+)
+    
+executor_comando_sql = connector.cursor() # também é necessário reiniciar a variável pois também senão dá o erro ReferenceError: weakly-referenced object no longer exists já que a variável anterior se referia apenas ao uso para procurar o banco de dados 
+executor_comando_sql.execute(f'SELECT COUNT(*) FROM information_schema.TABLES WHERE TABLE_NAME = "{tb_pedidos}";')
+resultado_tb = executor_comando_sql.fetchone()[0]
+connector.close()
+
+if resultado_tb > 0:
+    print(f"tabela {tb_pedidos} já existe!")
+else:
+    print("criando tabela")
+    connector = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        database= nome_db 
+    )
+    executor_comando_sql = connector.cursor()
+    executor_comando_sql.execute('CREATE TABLE pedidos (id INT AUTO_INCREMENT PRIMARY KEY, data DATETIME NOT NULL, tamanho VARCHAR(255),quantidade VARCHAR(255), valor_total DECIMAL(10,2) NOT NULL);')
+    connector.commit()
+    connector.close()
+
 
 tamanho_pizzas = ["Pequena", "Média", "Grande", "Familia"]
 precos_pizzas = {"Pequena": 30, "Média": 50, "Grande": 70, "Familia": 80}
