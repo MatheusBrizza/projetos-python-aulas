8e5c3be3e4d8a66676039f0783d0204fbe97399b
182300645 <matheusbj95@gmail.com>
2024-06-27 21:30:18 -0300

    feat: Exercício flask com conexão de banco de dados em andamento

---
+++ aula 52 - 25-06/Exercícios/config_bd.py
@@ -0,0 +1,66 @@
+import mysql.connector
+
+nome_db = "senac_pizzaria"
+tb_contatos = "contatos"
+def conectarBD():
+    connector = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password=''
+    )
+
+    executor_comando_sql = connector.cursor()
+    executor_comando_sql.execute(f'SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = "{nome_db}";')
+
+    # Salvando o resultado da pesquisa acima numa variável
+    resultado_db = executor_comando_sql.fetchone()[0]
+
+    # Fecha a conexão com o banco de dados
+    connector.close()
+
+    # Se o número de resultados for maior que zero, significa que o banco de dados existe
+    if resultado_db > 0:
+        print(f'O banco de dados {nome_db} existe e esta pronto para uso.')
+    else:
+        print(f"banco de dados {nome_db} não encontrado, criando-o agora.")
+        # Conectar-se ao servidor MySQL para criar o banco de dados
+        connector = mysql.connector.connect(
+            host='127.0.0.1',
+            user='root',
+            password=''
+        )
+
+        executor_comando_sql = connector.cursor() # necessário reiniciar a variável pois senão dá o erro ReferenceError: weakly-referenced object no longer exists pois a conexão foi terminada na utilização anterior
+        executor_comando_sql.execute(f'CREATE DATABASE {nome_db}')
+        connector.commit()
+        
+
+    # Conectar-se ao banco de dados senac_pizzaria recém-criado
+    connector = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        database= nome_db  
+    )
+        
+    # verificando se já existe a tabela pedidos
+    executor_comando_sql = connector.cursor() # também é necessário reiniciar a variável pois também senão dá o erro ReferenceError: weakly-referenced object no longer exists já que a variável anterior se referia apenas ao uso para procurar o banco de dados 
+    executor_comando_sql.execute(f"SELECT EXISTS (SELECT * FROM information_schema.tables WHERE table_schema = '{nome_db}'  AND table_name = '{tb_contatos}');")
+    resultado_tb = executor_comando_sql.fetchone()[0]
+    connector.close()
+
+    if resultado_tb > 0:
+        print(f"tabela {tb_contatos} já existe!")
+    else:
+        print("criando tabela contatos")
+        connector = mysql.connector.connect(host='127.0.0.1',user='root',password='',database= nome_db)
+        executor_comando_sql = connector.cursor()
+        executor_comando_sql.execute('CREATE TABLE contatos (id INT AUTO_INCREMENT PRIMARY KEY,nome VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,mensagem TEXT NOT NULL);')
+        connector.commit()
+        connector.close()
+    try:
+        connector = mysql.connector.connect(host='127.0.0.1',user='root',password='',database= nome_db)
+    except mysql.connector.Error as e:
+        print("Erro de conexão com banco de dados:", e)
+        raise
+    return connector
--- aula 52 - 25-06/Exercícios/exercicio.py
+++ aula 52 - 25-06/Exercícios/exercicio.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
+from config_bd import conectarBD
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "segredo"
+
+class FormularioContato(FlaskForm):
+    nome = StringField('Nome:', validators=[validators.DataRequired()])
+    email = StringField('Email:', validators=[validators.DataRequired(), validators.Email()])
+    mensagem = StringField('Mensagem:', validators=[validators.DataRequired()])
 
 @app.route('/')
 def index():
@@ -10,6 +20,30 @@ def index():
 def sobre():
     return render_template("sobre.html")
 
-@app.route('/contato')
+@app.route('/contato', methods=['GET', 'POST'])
 def contato():
+    form = FormularioContato()
+    
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        mensagem = form.mensagem.data
+        
+        try:
+            connector = conectarBD()
+            
+            executor_sql = connector.cursor()
+            comando_sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (%s, %s, %s)"
+            values = (nome, email, mensagem)
+            executor_sql.execute(comando_sql, values)
+            connector.commit()
+            
+            print("Salvo com sucesso!")
+        except connector.connector.Error as e:
+            print(f"Falha ao salvar dados! {e}")
+            
+    
     return render_template("contato.html")
+
+if __name__ == '__main__':
+    app.run
