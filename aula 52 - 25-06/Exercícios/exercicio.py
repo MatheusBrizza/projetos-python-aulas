e216d58136583dc3ea2866186edbba696cba0168
182300645 <matheusbj95@gmail.com>
2024-07-03 21:43:03 -0300

    feat: função de login funcionando + função editar usuário em andamento

--- aula 52 - 25-06/Exercícios/exercicio.py
+++ aula 52 - 25-06/Exercícios/exercicio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 from config_bd import conectarBD
@@ -12,14 +12,32 @@ class FormularioContato(FlaskForm):
     email = StringField('Email:', validators=[validators.DataRequired(), validators.Email()], render_kw={"placeholder":"Email"})
     mensagem = StringField('Mensagem:', validators=[validators.DataRequired()], render_kw={"placeholder":"Mensagem"})
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/', methods=['GET', 'POST'])
+def pagina_login():
     session.clear()
     session.pop('usuario_id', None)
-    #continuar
+    return render_template("login.html")
     
+@app.route('/validarlogin', methods=["POST", "GET"])
+def validar_login():
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    
+    connector = conectarBD()
+    
+    executor_sql = connector.cursor()
+    executor_sql.execute("""SELECT * FROM usuarios WHERE nome = %s AND senha = %s """, (nome, senha,))
+    usuario = executor_sql.fetchone()
+    executor_sql.close()
+    connector.close()
+    
+    if usuario:
+        session['usuario_id'] = usuario[0]
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('pagina_login'))
 
-@app.route('/')
+@app.route('/paginainicial')
 def index():
     return render_template("index.html")
 
@@ -35,7 +53,7 @@ def contato():
         nome = form.nome.data
         email = form.email.data
         mensagem = form.mensagem.data
-        
+
         try:
             connector = conectarBD()
             
@@ -74,6 +92,59 @@ def contatos():
     executor_sql.execute('SELECT * FROM contatos')
     contatos = executor_sql.fetchall()
     return render_template("contatos.html", contatos=contatos)
+
+@app.route('/usuarios')
+def usuarios():
+    if not session.get('usuario_id'):
+        return redirect(url_for('pagina_login'))
+    connector = conectarBD()
+    executor_sql = connector.cursor()
+    executor_sql.execute('SELECT * FROM usuarios')
+    usuarios = executor_sql.fetchall()
+    return render_template("usuarios.html", usuarios=usuarios)
+
+@app.route('/editarusuario/<id>', methods=['GET', 'POST'])
+def editarusuario(id):
+    if not id.isdigit():
+        return render_template('editarusuario/<id>', error='ID inválido')
+    
+    connector = conectarBD()
+    executor_sql = connector.cursor()
+    executor_sql.execute("""SELECT id, nome, email FROM usuarios WHERE id = %s;""", (id,))
+    dados_usuario = executor_sql.fetchone()
+    executor_sql.close()
+    connector.close()
+    
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        if not nome:
+            flash('O nome é obrigatório.')
+            return render_template('editarusuario/<id>', dados_usuario=dados_usuario)
+        if not email:
+            flash('O e-mail é obrigatório.')
+            return render_template('editarusuario/<id>', dados_usuario=dados_usuario)
+        if not senha:
+            flash('A senha é obrigatória.')
+            return render_template('editarusuario/<id>', dados_usuario=dados_usuario)
+        
+        connector = conectarBD()
+        executor_sql = connector.cursor()
+        comando_sql = 'UPDATE usuarios SET nome = %s, email = %s, senha = %s WHERE id = %s;'
+        valores = (nome, email, senha, id)
+        executor_sql(comando_sql, valores)
+        connector.commit()
+        executor_sql.close()
+        connector.close()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('editarusuario.html', id=id, dados_usuario=dados_usuario)
+        
+       
+
 
 if __name__ == '__main__':
     app.run
